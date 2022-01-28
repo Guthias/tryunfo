86cfe0190eeb401e5668b8435d82e2625204e1b6
unknown <gmgustavom@gmail.com>
2022-01-28 16:03:19 -0300

    add: adding props for Input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 export default class Input extends Component {
   render() {
+    const { title, id } = this.props;
+
     return (
       <div>
-        <label htmlFor="nome-do-input">
-          Titulo do Input
-          <input type="text" />
+        <label htmlFor={ id }>
+          { title }
+          <input type="text" id={ id } data-testid={ `${id}-input` } />
         </label>
       </div>
     );
