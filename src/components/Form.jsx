0a80c2981eb4a50e1d6b54377b2a4946239e800c
unknown <gmgustavom@gmail.com>
2022-01-28 17:48:26 -0300

    add: rendering selectOptions and fixing props validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,29 @@ import Input from './Input';
 import Select from './Select';
 
 export default class Form extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      selectOptions: [
+        {
+          value: 'normal',
+          text: 'Normal',
+        },
+        {
+          value: 'rare',
+          text: 'Raro',
+        },
+        {
+          value: 'veryRare',
+          text: 'Muito Raro',
+        },
+      ],
+    };
+  }
+
   render() {
+    const { selectOptions } = this.state;
     return (
       <form>
         <Input title="Nome" id="name" />
@@ -12,7 +34,7 @@ export default class Form extends Component {
         <Input type="number" title="Attr02" id="attr2" />
         <Input type="number" title="Attr03" id="attr3" />
         <Input title="Imagem" id="image" />
-        <Select title="Raridade" id="rare" />
+        <Select title="Raridade" id="rare" options={ selectOptions } />
       </form>
     );
   }
--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Select extends Component {
   render() {
@@ -22,8 +23,8 @@ export default class Select extends Component {
 Select.propTypes = {
   id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  options: PropTypes.isArrayOf(PropTypes.shape({
-    value: PropType.string.isRequired,
-    text: PropType.string.isRequired,
+  options: PropTypes.arrayOf(PropTypes.shape({
+    value: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
   })).isRequired,
 };
