1308fef1ed53af79746be9d373fe9704532c6631
unknown <gmgustavom@gmail.com>
2022-01-28 16:21:23 -0300

    add: adding type as prop on form Inputs

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,10 +6,10 @@ export default class Form extends Component {
     return (
       <form>
         <Input title="Nome" id="name" />
-        <Input title="Descrição" id="description" />
-        <Input title="Attr01" id="attr1" />
-        <Input title="Attr02" id="attr2" />
-        <Input title="Attr03" id="attr3" />
+        <Input type="textarea" title="Descrição" id="description" />
+        <Input type="number" title="Attr01" id="attr1" />
+        <Input type="number" title="Attr02" id="attr2" />
+        <Input type="number" title="Attr03" id="attr3" />
         <Input title="Imagem" id="image" />
       </form>
     );
