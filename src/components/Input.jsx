4fbe80259f05fba63c14ded414b3e04767023c46
unknown <gmgustavom@gmail.com>
2022-01-29 14:00:14 -0300

    add: adding inline props for Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class Input extends Component {
+  isInline(inline) {
+    if (inline === true) return 'inline';
+  }
+
   render() {
-    const { title, id, type } = this.props;
+    const { title, id, type, inline } = this.props;
 
     return (
       <div>
-        <label className="create-card-label" htmlFor={ id }>
+        <label className={ `create-card-label ${isInline(inline)}` } htmlFor={ id }>
           { title }
 
           {
             type === 'textarea'
-              ? <textarea className="create-card-textarea" id={ id } data-testid={ `${id}-input` } />
+              ? (
+                <textarea
+                  className="create-card-textarea"
+                  id={ id }
+                  data-testid={ `${id}-input` }
+                />)
               : (
                 <input
                   className="create-card-input"
@@ -29,10 +38,12 @@ export default class Input extends Component {
 
 Input.defaultProps = {
   type: 'text',
+  inline: false,
 };
 
 Input.propTypes = {
   type: PropTypes.string,
   id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
+  inline: PropTypes.bool,
 };
