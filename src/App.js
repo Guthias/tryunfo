1c477054c08829475b8542a28a047f99d0d38d58
unknown <gmgustavom@gmail.com>
2022-01-30 18:00:49 -0300

    add: sending props to Forms

--- src/App.js
+++ src/App.js
@@ -4,10 +4,56 @@ import Card from './components/Card';
 import './index.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '',
+      cardAttr2: '',
+      cardAttr3: '',
+      cardImage: '',
+      cardRare: 'normal',
+      cardTrunfo: false,
+      isSaveButtonDisabled: false,
+    };
+  }
+
+  onInputChange = ({ target }) => {
+    console.log(target.value);
+  }
+
+  onSaveButtonClick = ({ target }) => {
+    console.log(target);
+  }
+
   render() {
+    const { cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      isSaveButtonDisabled } = this.state;
+
     return (
       <div>
-        {/* <Form /> */}
+        <Form
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
+          onInputChange={ this.onInputChange }
+          onSaveButtonClick={ this.onSaveButtonClick }
+        />
         <Card />
       </div>
     );
